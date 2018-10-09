c3b32338d1edb0ddd9b7eb172a1702f1dcabe0ff
Marc Intha <marc.inthaamnouay@gmail.com>
2018-10-09 12:33:00 +0200

    :construction: project page

--- src/pages/Project.vue
+++ src/pages/Project.vue
@@ -1,34 +1,43 @@
 <template>
-  <div class="project-template">
-    <div id="project-type">
-      <h2>{{project.name}}</h2>
-      <div>
-        <p v-bind:class="project.type">{{project.type}}</p>
+  <div id="project-wrapper-view">
+    <div class="project-template">
+      <div id="project-type">
+        <h2>{{project.name}}</h2>
+        <div>
+          <p v-bind:class="project.type">{{project.type}}</p>
+        </div>
+      </div>
+      <div id="desc">
+        <h2>Description</h2>
+        <p>{{project.deepDesc}}</p>
+      </div>
+      <div id="lang">
+        <h2>Languages</h2>
+        <span
+          v-for="lang in project.languages"
+          v-bind:key="lang"
+          v-bind:class="lang"
+        >
+          {{lang}}
+        </span>
+      </div>
+      <div id="framework" v-if="project.misc">
+        <h2>Framework / Lib</h2>
+        <span
+          v-for="f in project.misc"
+          v-bind:key="f"
+          v-bind:class="f"
+        >
+          {{f}}
+        </span>
+      </div>
+      <div id="link">
+        <h2>Link</h2>
+        <a v-bind:href="project.link">Github</a>
       </div>
     </div>
-    <div id="desc">
-      <h2>Description</h2>
-      <p>{{project.deepDesc}}</p>
-    </div>
-    <div id="lang">
-      <h2>Languages</h2>
-      <span
-        v-for="lang in project.languages"
-        v-bind:key="lang"
-        v-bind:class="lang"
-      >
-        {{lang}}
-      </span>
-    </div>
-    <div id="framework" v-if="project.misc">
-      <h2>Framework</h2>
-      <span
-        v-for="f in project.misc"
-        v-bind:key="f"
-        v-bind:class="f"
-      >
-        {{f}}
-      </span>
+    <div id="img">
+      <img :src="image" />
     </div>
   </div>
 </template>
@@ -40,8 +49,11 @@ export default {
   name: 'project',
   data: function() {
     const id = this.$route.params.id
+    const project = getProjectById(id)
+
     return {
-      project: getProjectById(id)
+      project,
+      image: require(`../assets/img/${project.img}`)
     }
   },
   mounted: function() {
@@ -58,6 +70,10 @@ export default {
 <style lang="scss">
   @import '../assets/scss/lang';
   @import '../assets/scss/framework';
+
+  #project-wrapper-view {
+    display: flex;
+  }
 
   .project-template {
     padding: 20px;
@@ -91,6 +107,19 @@ export default {
         margin-left: 5px;
         margin-right: 5px;
       }
+    }
+
+    #link {
+      a {
+        color: #091123;
+      }
+    }
+  }
+
+  #img {
+    padding: 20px;
+    img {
+      width: 100%;
     }
   }
 
